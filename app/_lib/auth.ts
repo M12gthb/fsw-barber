a32b052275e68ff59cc92c44bc199edc7a893b0f
MATHEUS BARROS RODRIGUES <mat43214@gmail.com>
2024-02-08 14:23:31 -0300

    feat: auth next

--- app/_lib/auth.ts
+++ app/_lib/auth.ts
@@ -2,7 +2,7 @@ import { db } from "@/app/_lib/prisma";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { Adapter } from "next-auth/adapters";
 import GoogleProvider from "next-auth/providers/google";
-import NextAuth, { AuthOptions } from "next-auth";
+import { AuthOptions } from "next-auth";
 
 export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(db) as Adapter,
